e92fc32f1d86fe7559dbaee6440dbec2f259e6d5
Querijn Heijmans <heijmans.q@buas.nl>
2021-10-05 14:15:49 +0200

    Fix styling

--- Assets/Scripts/Interface/Plans Monitor/Plans/PlansGroupBar.cs
+++ Assets/Scripts/Interface/Plans Monitor/Plans/PlansGroupBar.cs
@@ -6,34 +6,34 @@ using TMPro;
 public class PlansGroupBar : MonoBehaviour
 {
 
-    public TextMeshProUGUI title;
-    public GameObject plansContainerOuter;
+	public TextMeshProUGUI title;
+	public GameObject plansContainerOuter;
 	public RectTransform plansContainerOuterRect;
 	public Transform plansContainerInner;
-    public Button foldButton;
+	public Button foldButton;
 	public RectTransform foldButtonRect;
 	public AddTooltip tooltip;
 
-    void Awake()
-    {
-        foldButton.onClick.AddListener(ToggleContent);
-        plansContainerOuterRect = plansContainerOuter.GetComponent<RectTransform>();
-        foldButtonRect.gameObject.SetActive(plansContainerOuterRect.rect.height != 0);
-    }
+	void Awake()
+	{
+		foldButton.onClick.AddListener(ToggleContent);
+		plansContainerOuterRect = plansContainerOuter.GetComponent<RectTransform>();
+		foldButtonRect.gameObject.SetActive(plansContainerOuterRect.rect.height != 0);
+	}
 
-    public void ToggleContent()
-    {
+	public void ToggleContent()
+	{
 		// Ignore empty content
 		if (plansContainerOuterRect.rect.height == 0)
-            return;
+			return;
 
 		plansContainerOuter.SetActive(!plansContainerOuter.activeSelf);
 
-        Vector3 rot = foldButtonRect.eulerAngles;
-        foldButtonRect.eulerAngles = (rot.z == 0) ? new Vector3(rot.x, rot.y, 90f) : new Vector3(rot.x, rot.y, 0f);
-    }
+		Vector3 rot = foldButtonRect.eulerAngles;
+		foldButtonRect.eulerAngles = (rot.z == 0) ? new Vector3(rot.x, rot.y, 90f) : new Vector3(rot.x, rot.y, 0f);
+	}
 
-    public void AddPlan(PlanBar plan)
+	public void AddPlan(PlanBar plan)
 	{
 		plan.transform.SetParent(plansContainerInner, false);
 		foldButtonRect.gameObject.SetActive(plansContainerOuterRect.rect.height != 0);
