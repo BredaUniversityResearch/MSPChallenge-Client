1f29facfa77ac124feacd66370c93496008ac6e2
Marin <hekman.m@buas.nl>
2022-02-25 14:50:16 +0100

    clean-up of unused uses

--- Assets/Scripts/Interface/Options/Options.cs
+++ Assets/Scripts/Interface/Options/Options.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Options : MonoBehaviour
