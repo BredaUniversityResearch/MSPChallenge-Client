248f09fe0bfc57cdf2fd0708be8cab4d930d39d6
Kevin <xiopize@gmail.com>
2022-05-30 16:10:59 +0200

    MSP-3480: Home, intro and part of the sessions screen

---
+++ Assets/Scripts/Interface/LogInMenu/LoginBGMask.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace MSP2050.Scripts
+{
+	public class LoginBGMask : MonoBehaviour
+	{
+		[SerializeField] private RectTransform m_bgRect;
+		[SerializeField] private RectTransform m_mask;
+		[SerializeField] private RectTransform m_maskedImage;
+		private Canvas m_canvas;
+		private bool m_initialized;
+
+		void Update()
+		{
+			if (!m_initialized)
+			{
+				m_initialized = true;
+				m_canvas = GetComponentInParent<Canvas>();
+				Image bg = m_bgRect.GetComponent<Image>();
+				RectTransform canvasRect = m_canvas.transform as RectTransform;
+				float bgAspect = (float)bg.sprite.texture.width / (float)bg.sprite.texture.height;
+				float canvasAspect = canvasRect.sizeDelta.x / canvasRect.sizeDelta.y;
+
+				float height, width;
+				if (bgAspect > canvasAspect)
+				{
+					//Match height
+					height = canvasRect.sizeDelta.y;
+					width = height * bgAspect;
+					m_bgRect.sizeDelta = new Vector2(width, height);
+					//m_bgRect.sizeDelta = new Vector2((width - canvasRect.sizeDelta.x) / 2f, 0f);
+				}
+				else
+				{
+					//Match width
+					width = canvasRect.sizeDelta.x;
+					height = width / bgAspect;
+					m_bgRect.sizeDelta = new Vector2(width, height);
+					//m_bgRect.sizeDelta = new Vector2(0f, (height - canvasRect.sizeDelta.y) / 2f);
+				}
+
+				m_maskedImage.sizeDelta = new Vector2(width, height);
+			}
+
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(m_canvas.transform as RectTransform, Input.mousePosition, m_canvas.worldCamera, out var localMousePos);
+			m_mask.anchoredPosition = new Vector2(0f, localMousePos.y); 
+			m_maskedImage.anchoredPosition = -m_mask.anchoredPosition;
+		}
+	}
+}
