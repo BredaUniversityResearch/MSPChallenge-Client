ce9cb73b65875d76e22bb31e4866a45b1b051d63
Jacopo <fabrini.j@buas.nl>
2022-08-22 09:49:30 +0200

    bugfix/MSP-3791 Using git tag as revision version
    Singleton test

--- Assets/Scripts/Utility/ApplicationBuildIdentifier.cs
+++ Assets/Scripts/Utility/ApplicationBuildIdentifier.cs
@@ -5,79 +5,114 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-	/// <summary>
-	/// Small scriptable object that acts as the build identifier. Contains information about the build date.
-	/// </summary>
-	[CreateAssetMenu]
-public class ApplicationBuildIdentifier : ScriptableObject
+public class ApplicationBuildIdentifier : MonoBehaviour
 {
-	//Actual file reside in Assets/Resources/
-	private const string BUILD_IDENTIFIER_ASSET_PATH = "BuildIdentifier";
-
-	[SerializeField] private string buildTime;
-	[SerializeField] private string gitTag;
+	private static ApplicationBuildIdentifier singleton;
+	public static ApplicationBuildIdentifier Instance
+	{
+		get
+		{
+			if (singleton == null)
+				singleton = FindObjectOfType<ApplicationBuildIdentifier>();
+			return singleton;
+		}
+	}
+	private string buildTime = "2022-08-24 13:24:12Z";
+	private string gitTag = "";
+	private bool hasInformation = false;
 
 #if UNITY_CLOUD_BUILD
 public static void UpdateBuildInformation(UnityEngine.CloudBuild.BuildManifestObject manifest)
 {
-    ApplicationBuildIdentifier identifier = Resources.Load<ApplicationBuildIdentifier>(BUILD_IDENTIFIER_ASSET_PATH);
-    identifier.UpdateBuildTime();
-    manifest.SetValue("buildNumber", identifier.gitTag);
-    manifest.SetValue("buildStartTime", identifier.buildTime);
+    manifest.SetValue("buildNumber", UpdateTag());
+    manifest.SetValue("buildStartTime", UpdateTime());
 }
 #endif
 
-	public static ApplicationBuildIdentifier FindBuildIdentifier()
+	void Awake()
 	{
-		ApplicationBuildIdentifier identifier = Resources.Load<ApplicationBuildIdentifier>(BUILD_IDENTIFIER_ASSET_PATH);
-		UnityCloudBuildManifest manifest = UnityCloudBuildManifest.Load();
-
-		if(manifest != null)
+		if (singleton != null && singleton != this)
 		{
-			identifier.buildTime = manifest.GetBuildTime();
-			identifier.gitTag = manifest.GetGitTag();
+			Destroy(this);
+			return;
+		}
+		else
+		{
+			singleton = this;
+			DontDestroyOnLoad(gameObject);
 		}
 
-		return identifier;
+		singleton.GetUCBManifest();
 	}
 
-	public void UpdateBuildTime()
+	public static string UpdateTime()
 	{
-		buildTime = System.DateTime.Now.ToString("u", CultureInfo.InvariantCulture);
-		gitTag = RunGitCommand();
+		string buildTime = System.DateTime.Now.ToString("u", CultureInfo.InvariantCulture);
+        return buildTime;
 	}
 
-	public string RunGitCommand()
+	public static string UpdateTag()
 	{
-		string result = "";
+        string gitTag = "";
+
 		var proc = new Process
 		{
 			StartInfo = new ProcessStartInfo()
 			{
 				FileName = "git",
-				Arguments = $"git describe --tags `git rev-list --tags --max-count=1`",
+				Arguments = "rev-list --tags --max-count=1",
 				UseShellExecute = false,
 				RedirectStandardOutput = true,
-				CreateNoWindow = true,
+			}
+		};
+
+		proc.Start();
+		string commitId = proc.StandardOutput.ReadToEnd();
+
+		proc = new Process
+		{
+			StartInfo = new ProcessStartInfo()
+			{
+				FileName = "git",
+				Arguments = $"describe --tags \"{commitId.Trim()}\"",
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
 			}
 		};
 
 		proc.Start();
 		while (!proc.StandardOutput.EndOfStream)
 		{
-			result += $"{proc.StandardOutput.ReadLine()},";
+			gitTag += $"{proc.StandardOutput.ReadLine()},";
 		}
 		proc.WaitForExit();
-		return result;
+
+		gitTag = gitTag.Remove(gitTag.Length - 1);
+
+		return gitTag;
+	}
+
+
+	public void GetUCBManifest()
+	{
+		UnityCloudBuildManifest manifest = UnityCloudBuildManifest.Load();
+
+		gitTag = manifest.buildNumber;
+		buildTime = manifest.buildStartTime;
+		hasInformation = true;
 	}
 
 	public string GetBuildTime()
-	{
+    {
 		return buildTime;
-	}
-
+    }
 	public string GetGitTag()
 	{
 		return gitTag;
 	}
+	public bool GetHasInformation()
+    {
+		return hasInformation;
+    }
+
 }
