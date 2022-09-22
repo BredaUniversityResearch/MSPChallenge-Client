044eb152251bd18da3a473bb13d03c7ec8fa3125
Kevin <hutchinson.k@buas.nl>
2022-09-22 16:52:55 +0200

    MSP-4142: initial version of active plan window

---
+++ Assets/Scripts/Interface/Layout/SetLayoutToPrefHeight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MSP2050.Scripts
+{
+	public class SetLayoutToPrefHeight : MonoBehaviour
+	{
+		[SerializeField] private LayoutGroup m_targetGroup = null;
+		[SerializeField] private LayoutElement m_targetElement = null;
+
+		private float oldHeight;
+
+		void Awake()
+		{
+			oldHeight = m_targetGroup.preferredHeight;
+		}
+
+		void Update()
+		{
+			float newHeight = m_targetGroup.preferredHeight;
+			if (!Mathf.Approximately(newHeight, oldHeight))
+			{
+				m_targetElement.preferredHeight = newHeight;
+			}
+			oldHeight = newHeight;
+		}
+	}
+}
