02e966a89f789f89b6e6a60ba516e4df3221b95a
Marin <89514754+marinhekman@users.noreply.github.com>
2023-03-21 19:37:21 +0100

    hide the networking blocker if the attempt lock fails otherwise the client cannot do anything (#124)

--- Assets/Scripts/Interface/Active Plans Window/AP_StateSelect.cs
+++ Assets/Scripts/Interface/Active Plans Window/AP_StateSelect.cs
@@ -212,7 +212,9 @@ namespace MSP2050.Scripts
 					changedPlan.SubmitState(newState, batch);
 					batch.ExecuteBatch(HideBlocker, SubmissionFailure);
 				}
-			}, null);
+			}, (_) => {
+				InterfaceCanvas.HideNetworkingBlocker();
+			});
 		}
 
 		void HideBlocker(BatchRequest a_batch)
