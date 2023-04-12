5de54b86d55fd1b8dd7fe725d1c0148557d56304
Kevin <xiopize@gmail.com>
2023-04-12 11:17:03 +0200

    Finished objectives monitor update

--- Assets/Scripts/Interface/Objectives/Monitor/ObjectivesMonitor.cs
+++ Assets/Scripts/Interface/Objectives/Monitor/ObjectivesMonitor.cs
@@ -24,6 +24,7 @@ namespace MSP2050.Scripts
 		[Header("Objectives")]
 		public MonitorObjective objectivePrefab;
 		public Transform objectiveLocation;
+		[SerializeField] GameObject noObjectivesText;
 		[SerializeField] NewObjectiveWindow newObjectivesWindow;
 		[SerializeField] Button newObjectivesButton;
 
@@ -152,6 +153,7 @@ namespace MSP2050.Scripts
 		private void FilterObjectives()
 		{
 			int selectedDeadlineMonth = filterEraDeadline.GetSelectedMonth();
+			bool objectiveVisible = false;
 			for (int i = 0; i < objectives.Count; i++)
 			{
 				if (objectives[i].TeamId != -1)
@@ -162,8 +164,10 @@ namespace MSP2050.Scripts
 					                (IsCompletionStateFilterEnabled(ECompletionStateFilter.InProgress) || details.completed) && 
 					                (selectedDeadlineMonth == -1 || selectedDeadlineMonth == details.deadlineMonth);
 					objectives[i].gameObject.SetActive(isActive);
+					objectiveVisible = objectiveVisible || isActive;
 				}
 			}
+			noObjectivesText.SetActive(!objectiveVisible);
 		}
 
 		private bool IsCompletionStateFilterEnabled(ECompletionStateFilter filterState)
