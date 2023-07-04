f4a621d0d790efd413d022032f76aaa8ab24a657
Jacopo <fabrini.j@buas.nl>
2023-07-04 16:47:58 +0200

    Revert "(Client, macOS only) Clicking on Read More under News in main/opening screen does not work"
    This reverts commit 83e1e69a5943ad007d99d3f585d391c97f6f7d0f.

--- Assets/Scripts/Interface/LogInMenu/LoginNewsEntry.cs
+++ Assets/Scripts/Interface/LogInMenu/LoginNewsEntry.cs
@@ -21,8 +21,7 @@ namespace MSP2050.Scripts
 
 		void Start()
 		{
-			m_readMoreButton.onClick.RemoveAllListeners();
-			m_readMoreButton.onClick.AddListener(()=> Application.OpenURL(m_moreInfoLink));
+			m_readMoreButton.onClick.AddListener(ReadMorePressed);
 		}
 
 		public void SetContent(LoginNewsData a_data)
@@ -35,9 +34,6 @@ namespace MSP2050.Scripts
 			m_readMoreText.margin = new Vector4(m_readMoreText.margin.x, m_readMoreText.margin.y, m_noImageMarginRight, m_readMoreText.margin.w);
 			//if (!string.IsNullOrEmpty(a_data.image_link))
 			//	StartCoroutine(DownloadImage(a_data.image_link));
-
-			m_readMoreButton.onClick.RemoveAllListeners();
-			m_readMoreButton.onClick.AddListener(() => Application.OpenURL(m_moreInfoLink));
 		}
 
 		public void SetImage(Texture2D a_texture)
@@ -67,10 +63,10 @@ namespace MSP2050.Scripts
 		//	}
 		//}
 
-		//void ReadMorePressed()
-		//{
-		//	Application.OpenURL(m_moreInfoLink);
-		//}
+		void ReadMorePressed()
+		{
+			Application.OpenURL(m_moreInfoLink);
+		}
 
 		public void FilterForSearch(string a_search)
 		{
