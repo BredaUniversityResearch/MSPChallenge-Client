d677eef5e1adc27eb3febff51d7426062f4cb33b
Kevin <hutchinson.k@buas.nl>
2024-06-11 09:35:46 +0200

    MSP-5079: Remove code giving errors

--- Assets/Scripts/Policies/EcologicalFishingGear/AP_EcoGear.cs
+++ Assets/Scripts/Policies/EcologicalFishingGear/AP_EcoGear.cs
@@ -16,27 +16,11 @@ namespace MSP2050.Scripts
 
 			//TODO: Get state for country at plan time
 			//TODO: get overrides of current plan
-			base.OpenToContent(a_content, a_toggle, a_APWindow);
-			if (a_content.TryGetPolicyData<PolicyPlanDataFishing>(PolicyManager.FISHING_POLICY_NAME, out var fishingData))
-			{
-				ecologyDistribution.SetSliderValuesToFishingDistribution(SimulationLogicMEL.Instance.GetFishingDistributionForPreviousPlan(a_content), fishingData.fishingDistributionDelta);
-				emptyContentOverlay.SetActive(ecologyDistribution.NumberGroups == 0);
-			}
-			else
-			{
-				Debug.LogError("Cannot get slider values for plan without fishing policy");
-			}
-			ecologyDistribution.SetInteractability(a_APWindow.Editing);
 		}
 
 		public override void ApplyContent()
 		{
 			//TODO: apply all toggle states, not just difference
-			if (m_APWindow.Editing)
-			{
-
-				ecologyDistribution.SetFishingToSliderValues(m_plan);
-			}
 		}
 	}
 }
