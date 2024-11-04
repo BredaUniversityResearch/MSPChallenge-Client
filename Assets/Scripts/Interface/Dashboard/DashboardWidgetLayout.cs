b3ee6689c7705488f758c5ed17bf71643af6c716
Kevin <hutchinson.k@buas.nl>
2024-11-04 10:23:52 +0100

    MSP-5084: Stash of graph setup

--- Assets/Scripts/Interface/Dashboard/DashboardWidgetLayout.cs
+++ Assets/Scripts/Interface/Dashboard/DashboardWidgetLayout.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 namespace MSP2050.Scripts
---
+++ Assets/Scripts/Interface/Dashboard/Graphs/AGraph.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MSP2050.Scripts
+{
+	public abstract class AGraph : MonoBehaviour
+	{
+		public abstract void SetData
+	}
+}
