437f51ebec5e010dbf87e9a3e091975f4c322b5d
Kevin <hutchinson.k@buas.nl>
2024-11-05 16:53:12 +0100

    MSP-5084: missing file

--- Assets/Scripts/Interface/Dashboard/Graphs/SteppedGraphBarGroup.cs
+++ Assets/Scripts/Interface/Dashboard/Graphs/SteppedGraphBarGroup.cs
@@ -42,7 +42,18 @@ namespace MSP2050.Scripts
 			}
 			else
 			{
-
+				for (int i = 0; i < a_data.m_categoryNames.Length; i++)
+				{
+					if (i < m_bars.Count)
+						m_bars.Add(Instantiate(m_barPrefab, m_barParent).GetComponent<SteppedGraphBarSingle>());
+					float value = a_data.m_steps[a_step][i];
+					float ymax = ymin + value / a_data.m_scale;
+					m_bars[i].SetData(a_data, a_step, i, 
+						i / a_data.m_categoryNames.Length, 
+						(i+1) / a_data.m_categoryNames.Length, 
+						0f, ymin, ymax);
+					ymin = ymax;
+				}
 			}
 			//TODO: set actual data
 		}
