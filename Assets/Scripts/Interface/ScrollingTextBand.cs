6c666ba5aaf44cb968a4097f106b6315753f8100
HutchinsonKevin <78085114+HutchinsonKevin@users.noreply.github.com>
2024-12-18 15:39:21 +0100

    Removed screen border gradient singleton (#244)
    * Removed screen border gradient singleton
    
    * Fix for scrolling text band and time manager control cover giving nullref
    
    ---------
    
    Co-authored-by: K_Hutchinson <xiopize@gmail.com>

--- Assets/Scripts/Interface/ScrollingTextBand.cs
+++ Assets/Scripts/Interface/ScrollingTextBand.cs
@@ -7,55 +7,22 @@ namespace MSP2050.Scripts
 
 		public float scrollSpeed;
 		public RectTransform textParent;
-		bool scrolling;
 		float childSize;
 		[SerializeField] float currentOffset;
 
-		private static ScrollingTextBand singleton;
-		public static ScrollingTextBand instance
+		void Start()
 		{
-			get
-			{
-				if (singleton == null)
-				{
-					singleton = FindObjectOfType<ScrollingTextBand>();
-				}
-				return singleton;
-			}
-		}
-
-		void Start ()
-		{
-			singleton = this;
 			childSize = textParent.GetChild(0).GetComponent<LayoutElement>().minWidth;
-			SetEnabled(false);
 		}
 
-		void Update () {
-			if (scrolling)
-			{
-				currentOffset += scrollSpeed * Time.deltaTime;
-				if (currentOffset > childSize)
-					currentOffset -= childSize;
-				//group.padding.left += (int)currentOffset;
-				textParent.anchoredPosition = new Vector2(-currentOffset, textParent.anchoredPosition.y);
-			}
-		}
-
-		public bool Scrolling
+		void Update()
 		{
-			set
-			{
-				currentOffset = 0;
-				scrolling = value;
-			}
-			get { return scrolling; }
-		}
+			currentOffset += scrollSpeed * Time.deltaTime;
+			if (currentOffset > childSize)
+				currentOffset -= childSize;
+			//group.padding.left += (int)currentOffset;
+			textParent.anchoredPosition = new Vector2(-currentOffset, textParent.anchoredPosition.y);
 
-		public void SetEnabled(bool value)
-		{
-			gameObject.SetActive(value);
-			Scrolling = value;
 		}
 	}
 }
